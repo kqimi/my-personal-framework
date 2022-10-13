13e93032907563231baa7b04ff89e5f4ea956fed
Kimi Li <qianbing@hawaii.edu>
2022-10-13 13:05:53 -1000

    added javascript smooth scroll

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // OPEN AND CLOSE SITE NAV MENU
+    const mysitenav= document.querySelector('.site-nav');
+    const mymenubutton = document.querySelector('.menu-button');
+
+    mymenubutton.onclick = function() {
+        // statements go here
+        if (mysitenav.getAttribute('data-navstate') === 'closed') {
+            mysitenav.setAttribute('data-navstate', 'open');
+        } else {
+            mysitenav.setAttribute('data-navstate', 'closed')
+        }
+    };
+});
